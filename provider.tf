4eebc3dbecb29325b5391dc731f5fe6fa9d8f9b9
Umesh Ranasinghe <umesh@wso2telco.com>
2024-01-02 18:25:55 +0530

    remove aws provider version

--- provider.tf
+++ provider.tf
@@ -2,14 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.74.2"
     }
   }
 }
 
 provider "aws" {
   region = "us-east-1"
-   assume_role {
-   role_arn = "xxx"
-  }
 }
